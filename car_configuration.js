38b271b5254e9f54dcc5f5173a1eb2b7a570c074
Maksym Samoilenko <Maksym_Samoilenko@epam.com>
2015-11-26 18:40:55 +0200

    Changed car_configuration.js

--- car_configuration.js
+++ car_configuration.js
@@ -1,4 +1,8 @@
-var car_configurations = "";
+var CAR_PIN_CONTANTS = "";
 var BASE_SKETCH_CONTENT = "void setup(){}void loop(){}";
-var PREDEFINED_RUN_MOTOR_FUNCTION = "";
-var PREDEFINED_FUNCTION_CONTENT = "void runMotor(bool isLeft, bool isForward, int powerPerPercent){}";
+var RUN_MOTOR_FOR_POWER_AND_TIME_FUNCTION = "void runMotor(bool isLeft, bool isForward, int powerPerPercent, int time){}";
+var RUN_MOTOR_FOR_DISTANCE_AND_POWER_FUNCTION = "void runMotor(bool isLeft, bool isForward, int powerPerPercent, int time){}";
+var TURN_N_GRAD_FUNNCTION = "void turn(int grad){}";
+var STOP_ALL_MOTORS_FUNCTION = "void stopAllMotors(){}";
+var STOP_SELECTED_MOTOR_FUNCTION = "void stopSelectedMotor(bool isLeft){}";
+var CHECK_IF_BARRIER_IS_CLOSE_THAN_N_SM_THEN_TURN_K_GRAD_FUNCTION = "void checkIfBarrierIsCloseThanNSmThenTurnKGrad(int distance, int grad){}";
