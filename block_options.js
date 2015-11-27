57f29283523c4cccd2c82ce156545dbc0fd95031
Maksym Samoilenko <Maksym_Samoilenko@epam.com>
2015-11-27 18:41:45 +0200

    Added block ui options.

--- block_options.js
+++ block_options.js
@@ -1,49 +1,280 @@
-var json_motor_option = {
-"id": "motor_option",
-"message0": "Оберіть мотор %1 Оберіть напрямок руху %2 Та задайте потужність %3 %4",
-"args0": [
-{
-"type": "field_dropdown",
-"name": "motor",
-"options": [
-[
-  "Лівий",
-  "left"
-],
-[
-  "Правий",
-  "right"
-]
-]
-},
-{
-"type": "field_dropdown",
-"name": "direction",
-"options": [
-[
-  "Вперед",
-  "move_front"
-],
-[
-  "Назад",
-  "move_back"
-]
-]
-},
-{
-"type": "field_input",
-"name": "FORCE",
-"text": "Потужність"
-},
-{
-"type": "input_statement",
-"name": "motor_option"
+var  run_motor_for_power_and_time_function = {
+  "id": "run_motor_for_power_and_time_function",
+  "message0": "Select %1 %2 with power %3 for %4 seconds",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "MOTOR",
+      "options": [
+        [
+          "left",
+          "LEFT"
+        ],
+        [
+          "right",
+          "RIGHT"
+        ]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "POWER",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "TIME",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 90,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
 }
-],
-"inputsInline": false,
-"previousStatement": null,
-"nextStatement": null,
-"colour": 20,
-"tooltip": "",
-"helpUrl": "http://www.example.com/"
+
+
+Blockly.JavaScript['run_motor_for_power_and_time_function'] = function(block) {
+  var dropdown_motor = block.getFieldValue('MOTOR');
+  var value_power = Blockly.JavaScript.valueToCode(block, 'POWER', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_time = Blockly.JavaScript.valueToCode(block, 'TIME', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'XinterpipeX'
+  return code;
 };
+
+
+Blockly.Blocks['run_motor_for_power_and_time_function'] = {
+  init: function() {
+    this.jsonInit(run_motor_for_power_and_time_function);
+  }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+var run_motor_for_distance_and_power_function = {
+  "id": "run_motor_for_distance_and_power_function",
+  "message0": "Select %1 %2 with power %3 for %4 sm",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "MOTOR",
+      "options": [
+        [
+          "left",
+          "LEFT"
+        ],
+        [
+          "right",
+          "RIGHT"
+        ]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "POWER",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "DISTANCE",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 90,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+};
+
+
+Blockly.JavaScript['run_motor_for_distance_and_power_function'] = function(block) {
+  var dropdown_motor = block.getFieldValue('MOTOR');
+  var value_power = Blockly.JavaScript.valueToCode(block, 'POWER', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_distance = Blockly.JavaScript.valueToCode(block, 'DISTANCE', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+
+Blockly.Blocks['run_motor_for_distance_and_power_function'] = {
+  init: function() {
+    this.jsonInit(run_motor_for_distance_and_power_function);
+  }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+
+var turn_n_grad_function = {
+  "id": "turn_n_grad_function",
+  "message0": "turn car  %1 grad",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "GRAD",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 15,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+};
+
+
+Blockly.JavaScript['turn_n_grad_function'] = function(block) {
+  var value_grad = Blockly.JavaScript.valueToCode(block, 'GRAD', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+
+Blockly.Blocks['turn_n_grad_function'] = {
+  init: function() {
+    this.jsonInit(turn_n_grad_function);
+  }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+
+var stop_all_motors_function = {
+  "id": "stop_all_motors_function",
+  "message0": "stop all motors",
+  "args0": [],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 300,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+}
+
+
+Blockly.JavaScript['stop_all_motors_function'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+
+Blockly.Blocks['stop_all_motors_function'] = {
+  init: function() {
+    this.jsonInit(stop_all_motors_function);
+  }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+
+var stop_selected_motor_function = {
+  "id": "stop_selected_motor_function",
+  "message0": "stop %1 motor",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "MOTOR",
+      "options": [
+        [
+          "left",
+          "LEFT"
+        ],
+        [
+          "right",
+          "RIGHT"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 300,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+};
+
+
+Blockly.JavaScript['stop_selected_motor_function'] = function(block) {
+  var dropdown_motor = block.getFieldValue('MOTOR');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+
+Blockly.Blocks['stop_selected_motor_function'] = {
+  init: function() {
+    this.jsonInit(stop_selected_motor_function);
+  }
+};
+
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
+
+
+var check_if_barrier_is_close_than_n_sm_then_turn_k_grad_function = {
+  "id": "check_if_barrier_is_close_than_n_sm_then_turn_k_grad_function",
+  "message0": "Check if barrier is close than  %1 then turn n grad %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "DISTANCE_SENSOR",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "TURN",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 255,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+};
+
+
+Blockly.JavaScript['check_if_barrier_is_close_than_n_sm_then_turn_k_grad_function'] = function(block) {
+  var value_distance_sensor = Blockly.JavaScript.valueToCode(block, 'DISTANCE_SENSOR', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_turn = Blockly.JavaScript.valueToCode(block, 'TURN', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+
+Blockly.Blocks['check_if_barrier_is_close_than_n_sm_then_turn_k_grad_function'] = {
+  init: function() {
+    this.jsonInit(check_if_barrier_is_close_than_n_sm_then_turn_k_grad_function);
+  }
+};
+
+//---------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
